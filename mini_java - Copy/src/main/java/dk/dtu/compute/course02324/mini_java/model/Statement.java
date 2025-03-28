bf0afd26576a3f3b290556ee54043154cfae2ec0
MoellerGutten <tobias-alleroed@hotmail.com>
2025-03-28 09:49:24 +0100

    Fix structure

--- mini_java - Copy/src/main/java/dk/dtu/compute/course02324/mini_java/model/Statement.java
+++
@@ -1,7 +0,0 @@
-package dk.dtu.compute.course02324.mini_java.model;
-
-import dk.dtu.compute.course02324.mini_java.infrastructure.VisitAcceptor;
-
-public interface Statement extends VisitAcceptor {
-
-}
